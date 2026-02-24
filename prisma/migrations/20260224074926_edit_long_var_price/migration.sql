da10b0bcc8a47cdd1724b4dc77a76d9d0ac849ea
mhdyousef1994-tech <mhdyousef1994@gmail.com>
2026-02-24 10:50:10 +0300

    add migration

---
+++ prisma/migrations/20260224074926_edit_long_var_price/migration.sql
@@ -0,0 +1,20 @@
+/*
+  Warnings:
+
+  - You are about to alter the column `total_amount` on the `invoice` table. The data in that column could be lost. The data in that column will be cast from `Decimal(12,2)` to `Decimal(20,2)`.
+  - You are about to alter the column `paid_amount` on the `invoice` table. The data in that column could be lost. The data in that column will be cast from `Decimal(12,2)` to `Decimal(20,2)`.
+  - You are about to alter the column `remaining_amount` on the `invoice` table. The data in that column could be lost. The data in that column will be cast from `Decimal(12,2)` to `Decimal(20,2)`.
+  - You are about to alter the column `unit_price` on the `orderitem` table. The data in that column could be lost. The data in that column will be cast from `Decimal(10,2)` to `Decimal(20,2)`.
+  - You are about to alter the column `price_per_meter` on the `pricecolor` table. The data in that column could be lost. The data in that column will be cast from `Decimal(10,2)` to `Decimal(20,2)`.
+
+*/
+-- AlterTable
+ALTER TABLE `invoice` MODIFY `total_amount` DECIMAL(20, 2) NOT NULL,
+    MODIFY `paid_amount` DECIMAL(20, 2) NOT NULL,
+    MODIFY `remaining_amount` DECIMAL(20, 2) NOT NULL;
+
+-- AlterTable
+ALTER TABLE `orderitem` MODIFY `unit_price` DECIMAL(20, 2) NULL;
+
+-- AlterTable
+ALTER TABLE `pricecolor` MODIFY `price_per_meter` DECIMAL(20, 2) NOT NULL;
